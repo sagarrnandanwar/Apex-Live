1bec462e2119fc59cf71a38bd80d97e029efe042
Tanishq Dhote <119649887+ptsdpatient@users.noreply.github.com>
2024-10-04 10:58:16 +0530

    recreating project for structure mismanagement

--- apexadmin/android/app/src/main/kotlin/com/apex/apexadmin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.apex.apexadmin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ app/android/app/src/main/kotlin/com/apex/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.apex.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
