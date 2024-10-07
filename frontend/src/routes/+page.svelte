32da890d1f51a365ccc9c681dc21681c4a47d204
Sagar Nandanwar <sagarnandanwar22@gmail.com>
2024-10-07 17:24:30 +0530

    frontend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,1 +1,194 @@
-camera
+<script>
+
+    import {onMount} from 'svelte'
+
+    let url='http://192.168.1.6:2000'
+
+    let token
+
+    const getToken = () =>{
+    return localStorage.getItem('authToken')
+    }
+
+    let pollingStationList=[]    
+    let cameraList=[]
+    let talukaList=[]
+    let cameraIndex=0
+    let selectedResolution ={
+        row : "3",
+        col : "4"
+    };
+    let resolutionList=[
+        {
+            r:"3",
+            c:"4"
+        },{
+            r:"2",
+            c:"2"
+        },{
+            r:"4",
+            c:"6"
+        },{
+            r:"6",
+            c:"12"
+        },
+    ]
+
+    async function authenticateToken() {
+        if (!token) {
+            window.location = '/login'; 
+            return;
+        }
+    
+        try {
+            const response = await fetch(`${url}/authenticateToken`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`, 
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            const data = await response.json();
+
+            if(data.done){
+                console.log('Authenticated user:', data);
+                if(data.error) window.location='/login'
+                isAdmin=data.isAdmin
+                alert("login successful")
+            }
+
+            if (response.ok) {
+                
+               console.log("real")
+            } else {
+                alert(data.error || 'Token is invalid');
+                window.location = '/login'; 
+            }
+        } catch (error) {
+            console.error('Error during token authentication:', error);
+            alert("An error occurred while authenticating the token.");
+        }
+    }
+
+
+
+    async function fetchPollingStations() {
+       
+       try {
+           const response = await fetch(`${url}/getPollingStation`, {
+               method: 'GET',
+               headers: {
+                   'Authorization': `Bearer ${token}`,
+                   'Content-Type': 'application/json'
+               }
+           });
+
+           pollingStationList = await response.json();
+       } catch (error) {
+           console.error('Error fetching employees:', error);
+           errorMessage = 'An error occurred while fetching employees.';
+       }
+   }
+
+   async function fetchCameras() {
+       
+       try {
+           const response = await fetch(`${url}/getCameras`, {
+               method: 'GET',
+               headers: {
+                   'Authorization': `Bearer ${token}`,
+                   'Content-Type': 'application/json'
+               }
+           });
+
+           cameraList = await response.json();
+       } catch (error) {
+           console.error('Error fetching employees:', error);
+           errorMessage = 'An error occurred while fetching employees.';
+       }
+   }
+
+   async function fetchTalukas() {
+       
+       try {
+           const response = await fetch(`${url}/getTalukas`, {
+               method: 'GET',
+               headers: {
+                   'Authorization': `Bearer ${token}`,
+                   'Content-Type': 'application/json'
+               }
+           });
+
+           talukaList = await response.json();
+       } catch (error) {
+           console.error('Error fetching employees:', error);
+           errorMessage = 'An error occurred while fetching employees.';
+       }
+   }
+
+   function handleResolution(resolution){
+    selectedResolution.row=resolution.r
+    selectedResolution.col=resolution.c
+   }
+
+   function getInfo(){
+    fetchPollingStations()
+    fetchTalukas()
+    fetchCameras()
+   }
+
+   onMount(()=>{
+    token=getToken()
+    // authenticateToken()
+    getInfo()
+   })
+
+</script>
+<div class="flex flex-col pb-2 pt-1 rounded-xl mx-auto bg-gray-100 transition-all duration-300" style="width:100%;height:100svh;">
+    <div class="w-full mx-auto h-full flex flex-col transition-all duration-300">
+        
+        <!-- ye button ke liye hai -->
+         
+        <div class="w-full flex transition-all duration-300 flex-row p-2 justify-between items-center align-center" style="height:8svh">
+            
+            <div class=" flex flex-row gap-6">
+                <select class=" rounded-xl border border-xl  px-3 py-1">
+                    {#each pollingStationList as poll}
+                        <option>{poll.polling_station}</option>
+                        
+                    {/each}
+                </select>
+                <select class="rounded-xl border border-xl  px-3 py-1  ">
+                    {#each talukaList as taluka}
+                        <option>{taluka.taluka}</option>
+                        
+                    {/each}
+                </select>
+            
+            </div>
+
+            <div class="flex flex-row gap-7">
+                <select class="rounded-xl border border-xl  px-3 py-1  " on:change="{(e) => handleResolution(resolutionList[e.target.selectedIndex])}">
+                    {#each resolutionList as resolution}
+                        <option value='{resolution.r} x {resolution.c}'>{resolution.r} x {resolution.c}</option>
+                    {/each}
+                </select>
+                <button class="rounded-xl border border-xl px-3 py-1">slide duration</button>
+            </div>
+
+        </div>
+         
+        <!-- ye camera ka hai -->
+        <div class="w-full transition-all duration-300 grid grid-rows-{selectedResolution.row} grid-cols-{selectedResolution.col} px-2 jusify-around  gap-2" style="height:92svh;">
+        
+        
+                {#each cameraList.slice(cameraIndex, cameraIndex + Number(selectedResolution.row) * Number(selectedResolution.col)) as camera}
+                    <button class="text-white bg-gray-700 px-1 py-1 flex flex-col text-left whitespace-nowrap overflow-hidden w-full text-ellipse justify-end items-left  transform hover:scale-95 rounded-lg transaction-all duration-300" >
+                        <div class="bg-blue-400 rounded-lg h-auto flex-grow w-full h-full"></div>
+                        <div>PS : 134 , CID : H4G178566 , Address : awpdldapldaplpalpdaldpa</div>
+                    </button>
+                {/each}
+        </div>
+    </div>
+</div>      
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,1 +1,80 @@
-login
+<script>
+    import {onMount} from 'svelte'
+    let username = '';
+    let password = '';
+    let url='http://192.168.1.6:2000'
+
+    async function loginUser() {
+        if (!username || !password) {
+            alert("Please enter both username and password");
+            return;
+        }
+
+        try {
+            const response = await fetch(`${url}/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: username,password: password })
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                // Store token in local storage
+                localStorage.setItem('authToken', data.token);
+                window.location.href = '/'; // Redirect to user page or dashboard
+            } else {
+                alert(data.error || 'Login failed');
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+            alert("An error occurred during login. Please try again.");
+        }
+    }
+
+
+    onMount(()=>{
+
+    })
+
+
+
+
+
+</script>
+
+<head>
+    <title>Login Page</title>
+</head>
+
+<body class="w-full h-full  flex flex-row items-center justify-end  bg-white" style="width:100vw;height:100vh;">
+
+    <div class="w-2/3 h-full flex flex-col ">
+        <img src="icon.jpg" alt="camera" class="w-4/5 mx-right">
+    </div>
+    <div class=" w-1/3 p-8 space-y-6 bg-gray-200 rounded-lg shadow-xl mr-10">
+        <h2 class="text-2xl font-bold text-center text-gray-800">Apex Live</h2>
+
+        <form class="space-y-4">
+            <div>
+                <label for="username" class="block text-xl font-medium text-gray-700">Username</label>
+                <input bind:value={username} type="username" id="username" name="username" required class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500" placeholder="">
+            </div>
+
+            <div>
+                <label for="password" class="block text-xl font-medium text-gray-700">Password</label>
+                <input bind:value={password} type="password" id="password" name="password" required class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-blue-500" placeholder="">
+            </div>
+
+            <button on:click={loginUser} class="mt-1 block w-1/3 mx-auto px-3 py-2 border border-gray-300 rounded-md text-white transform hover:scale-105 duration-300 transition-all shadow-sm focus:outline-none focus:ring bg-blue-500 focus:ring-blue-500">Login</button>
+        </form>
+
+        <p class="text-center text-sm text-gray-600">
+            
+            <a href="/help" class="font-medium text-blue-600 hover:text-blue-500">Help Desktop</a>
+        </p>
+    </div>
+
+</body>
